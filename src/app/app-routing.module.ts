1d4272c998d0aa82a49fe1f2a2360babdcbf7034
Ganeshsurya11599 <a.surya11599@gnail.com>
2019-07-26 00:42:12 -0700

    Childrouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { AboutComponent } from './about/about.component';
+import { IndexComponent } from './index/index.component';
+import { ServiceComponent } from './service/service.component';
+import { ElectronicsComponent } from './electronics/electronics.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent,
+  children:[
+    {path:'contactus',component:ContactusComponent},
+    {path:'aboutus',component:AboutusComponent}
+  ]
+},
+{path:'about',component:AboutComponent,
+children:[
+  {path:'index',component:IndexComponent}
+]
+},
+{path:'service',component:ServiceComponent,
+children:[
+{path:'electronics',component:ElectronicsComponent}
+]
+},
+{path:'',redirectTo:'/home.component.html',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ElectronicsComponent } from './electronics/electronics.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ServiceComponent,
+    ContactusComponent,
+    AboutusComponent,
+    ElectronicsComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
